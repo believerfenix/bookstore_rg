2bd1c061838fd73f9cb079f57bdf5f9301d54b28
believerfenix <us55554444us55554444@gmail.com>
2022-03-08 18:48:44 +0200

    add mvc for page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class PagesController < ApplicationController
+  # before_action :set_page, only: %i[show edit update destroy]
+
+  # GET /pages or /pages.json
+  def index; end
+end
