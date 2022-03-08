bcea241bf8aff713bcbf5704f2ba6a9e27e35c0f
believerfenix <us55554444us55554444@gmail.com>
2022-03-09 01:08:18 +0200

    add empty line. change view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,13 @@
 import $ from 'jquery';
 
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 window.$ = $;
 window.jQuery = $;
 
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 import 'bootstrap-sass/assets/javascripts/bootstrap';
