d0f78ffbc751ce7b570f7b038a32f107e7ca8992
believerfenix <us55554444us55554444@gmail.com>
2022-03-29 10:24:46 +0300

    change js file

--- app/javascript/src/books.js
+++ app/javascript/src/books.js
@@ -24,20 +24,20 @@ $(document).on('turbolinks:load', function(){
     $('.textControl').each(function() {
       var content = $(this).html();
       if(content.length > showChar) {
-          var c = content.substr(0, showChar);
-          var h = content.substr(showChar, content.length - showChar);
-          var html = c + '<span class="moreellipses">' + ellipsesText+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moreText + '</a></span>';
-          $(this).html(html);
+        var c = content.substr(0, showChar);
+        var h = content.substr(showChar, content.length - showChar);
+        var html = c + '<span class="moreellipses">' + ellipsesText+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moreText + '</a></span>';
+        $(this).html(html);
       }
     });
 
     $(".morelink").click(function(){
       if($(this).hasClass("less")) {
-          $(this).removeClass("less");
-          $(this).html(moreText);
+        $(this).removeClass("less");
+        $(this).html(moreText);
       } else {
-          $(this).addClass("less");
-          $(this).html(lessText);
+        $(this).addClass("less");
+        $(this).html(lessText);
       }
       $(this).parent().prev().toggle();
       $(this).prev().toggle();
