ae37c7df33f8cb21b36b6a8b01d8b08ad43654e7
believerfenix <us55554444us55554444@gmail.com>
2022-04-03 18:30:25 +0300

    fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,14 +2,14 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    FACEBOOK = 'Facebook'.freeze
-  
+    FACEBOOK = 'Facebook'
+
     def facebook
       @user = User.from_omniauth(request.env['omniauth.auth'])
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: FACEBOOK) if is_navigational_format?
     end
-  
+
     def failure
       redirect_to root_path
       set_flash_message(:error, :failure, kind: FACEBOOK) if is_navigational_format?
