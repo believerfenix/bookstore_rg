e3d35f137a14ecf83b82d2cc3d24cbe1975eb421
believerfenix <us55554444us55554444@gmail.com>
2022-04-03 23:19:30 +0300

    fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,6 @@ module Users
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication
-        #binding.pry
         set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
       else
         session['devise.facebook_data'] = request.env['omniauth.auth']
