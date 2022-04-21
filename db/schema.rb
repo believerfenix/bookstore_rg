7e536be128df633ae1b2559b5d48273b27a72a1f
believerfenix <us55554444us55554444@gmail.com>
2022-04-21 17:40:34 +0300

    added review migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_121120) do
+ActiveRecord::Schema.define(version: 2022_04_21_140335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,19 @@ ActiveRecord::Schema.define(version: 2022_04_18_121120) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "book_rate"
+    t.string "title"
+    t.text "body"
+    t.integer "status", default: 0
+    t.bigint "user_id"
+    t.bigint "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "user_addresses", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "address_id"
@@ -127,6 +140,8 @@ ActiveRecord::Schema.define(version: 2022_04_18_121120) do
 
   add_foreign_key "author_books", "authors"
   add_foreign_key "author_books", "books"
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
   add_foreign_key "user_addresses", "addresses"
   add_foreign_key "user_addresses", "users"
 end
