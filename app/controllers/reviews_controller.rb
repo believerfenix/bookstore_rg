60ea82caa5aec43dac8d1eeb1ec3a00124ea5364
believerfenix <us55554444us55554444@gmail.com>
2022-04-21 17:36:50 +0300

    added reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ReviewsController < ApplicationController
+  def create
+    review_form = ReviewForm.new(review_params)
+    if review_form.save(current_user)
+      flash[:success] = I18n.t('review.success')
+    else
+      flash[:danger] = review_form.errors.full_messages.to_sentence
+    end
+    redirect_to book_url(id: review_params[:book_id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :book_rate, :body, :book_id)
+  end
+end
