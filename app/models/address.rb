9da6ff69755b737894c6294c3c51b8aafb2e1967
believerfenix <us55554444us55554444@gmail.com>
2022-05-03 13:06:27 +0300

    fix address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Address < ApplicationRecord
-  has_many :user_addresses, dependent: :destroy
-  has_many :users, through: :user_addresses
+  belongs_to :user
 
   enum kind: { billing: 0, shipping: 1 }
 end
