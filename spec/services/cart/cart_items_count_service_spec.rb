3befd9cb2994f1f46292051d313e298f5b5994f7
believerfenix <us55554444us55554444@gmail.com>
2022-06-22 13:25:16 +0300

    changed service cart spec

--- spec/services/cart/cart_items_count_service_spec.rb
+++ spec/services/cart/cart_items_count_service_spec.rb
@@ -3,15 +3,13 @@
 RSpec.describe Cart::CartItemsCountService do
   include Devise::Test::IntegrationHelpers
 
-  subject { described_class.new(session).call }
+  subject { described_class.new(order).call }
 
   let(:order) { create(:order, order_items: order_items) }
   let(:order_items) { create_list(:order_item, 3) }
 
   describe '.call' do
     context 'with full_cart' do
-      let(:session) { { cart_id: order.id } }
-
       let(:expected_result) { order.order_items.sum(&:quantity) }
 
       it 'count cart items' do
@@ -20,7 +18,6 @@ RSpec.describe Cart::CartItemsCountService do
     end
 
     context 'with empty cart' do
-      let(:session) { { cart_id: order.id } }
       let(:order) { create(:order) }
 
       let(:expected_result) { 0 }
