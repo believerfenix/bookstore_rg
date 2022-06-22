bd35285b563a9f6262774bf3b44c0ae4b2675d16
believerfenix <us55554444us55554444@gmail.com>
2022-06-22 13:24:41 +0300

    changed requests spec

--- spec/requests/checkout_requests_spec.rb
+++ spec/requests/checkout_requests_spec.rb
@@ -1,17 +1,38 @@
 # frozen_string_literal: true
 
-RSpec.describe 'Checkout', type: :request do
+RSpec.describe 'Checkouts', type: :request do
+  let(:user) { create(:user) }
+  let(:order) { create(:order) }
+
   describe 'GET /checkout' do
-    context 'when user ready to make order' do
-      before { get checkout_path }
+    before do
+      sign_in(user)
+      get checkout_path
+    end
 
-      it 'returns http success' do
-        expect(response).to have_http_status(:ok)
-      end
+    it 'returns http success' do
+      expect(response).to have_http_status(:ok)
+    end
 
-      it 'renders show template' do
-        expect(response).to render_template(:show)
-      end
+    it 'renders show template' do
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'PUT /checkout' do
+    let(:order_params) { { billing_address: attributes_for(:address, kind: :billing), only_billing: true } }
+
+    before do
+      sign_in(user)
+      put checkout_path, params: { order: order_params }
+    end
+
+    it 'returns http redirect' do
+      expect(response).to have_http_status(:redirect)
+    end
+
+    it 'redirects to show page' do
+      expect(response).to redirect_to(checkout_path)
     end
   end
 end
