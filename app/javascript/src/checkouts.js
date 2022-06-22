d48a754bf96100fa1ca196a1c38c01d745f6cde2
believerfenix <us55554444us55554444@gmail.com>
2022-06-22 13:11:30 +0300

    added js/scss for checkout

---
+++ app/javascript/src/checkouts.js
@@ -0,0 +1,11 @@
+$(document).on('turbolinks:load', function(){
+  $('#order_only_billing').on('click', function() {
+      if($(this).is(":checked")) {
+        $('#shipping_address_form').hide()
+      }
+      else {
+          $('#shipping_address_form').show()
+      }
+   });
+
+})
